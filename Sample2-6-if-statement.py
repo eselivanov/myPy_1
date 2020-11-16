074fca3b9842802bd84a856a2ee05a5d5a032053
Selivanov, Eugene (MOH) <eugene.selivanov@ontario.ca>
2020-11-16 12:57:07 -0500

    Sample2-6

--- Sample2-6-if-statement.py
+++ Sample2-6-if-statement.py
@@ -1,10 +1,10 @@
 def fib(n):
-    print 'n =', n
+    print('n =', n)
     if n > 1:
-        return n * fib(n - 1)
+        return( n * fib(n - 1) )
     else:
-        print 'end of line'
-        return 1
-
-print fib(3)
+        print( 'end of line' )
+        return( 1 )
+#-----
+print(fib(3))
 
