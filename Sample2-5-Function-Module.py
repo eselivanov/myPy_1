c0129d9a80bb3e00645835e282d28daf7240e1f0
Selivanov, Eugene (MOH) <eugene.selivanov@ontario.ca>
2020-11-16 12:52:09 -0500

    sample2-5

--- Sample2-5-Function-Module.py
+++ Sample2-5-Function-Module.py
@@ -5,13 +5,14 @@ import odbchelper
 params = {"server":"mpilgrim", "database":"master", "uid":"sa", "pwd":"secret"}
 
 # call function from module odbchelper with dictionary of parameters
-print odbchelper.buildConnectionString(params)
+print( "params: ", odbchelper.buildConnectionString(params) )
 
 # call function for the doc string - every function has this attribute
-print odbchelper.buildConnectionString.__doc__
+print( "__doc__ : ", odbchelper.buildConnectionString.__doc__ )
 
 import sys # import built-in module; sys is an object and path - its attribute
-print sys.path # print system search path for modules
+print( "sys.path: ", sys.path, "\n" ) # print system search path for modules 
 
 # add new directory to the search path
-#sys.path.append('/my/new/path')
+sys.path.append('/my/new/path')
+print( "new sys.path: ", sys.path ) 
--- odbchelper.py
+++ odbchelper.py
@@ -1,19 +1,16 @@
 def buildConnectionString(params):
     """Build a connection string from a dictionary of parameters.
-
-    Returns string."""
+           Returns string."""
 
     # contruct string from the dictionary of parametrs
-    return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
+    return "; ".join(["%s=%s" % (k, v) for k, v in params.items()])
 
 
 # when module is used as a standalone program i.e. for debug:
-#
-#if __name__ == "__main__":
-#        myParams = {"server":"mpilgrim", \
-#                    "database":"master", \
-#                    "uid":"sa", \
-#                    "pwd":"secret" \
-#                    }
-
-#print buildConnectionString(myParams)
+if __name__ == "__main__":
+    myParams = {"server":"mpilgrim1", \
+                "database":"master1", \
+                "uid":"sa1", \
+                "pwd":"secret1" \
+                }
+    print( buildConnectionString(myParams) )
